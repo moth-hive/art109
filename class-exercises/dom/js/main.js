912fa3f9ac994e92823eb9188d7cac0b0b75c374
Solar-Moth <thaddeus.cobb@sjsu.edu>
2024-09-20 18:50:08 -0700

    added item fade in

--- class-exercises/dom/js/main.js
+++ class-exercises/dom/js/main.js
@@ -1,2 +1,12 @@
-console.log("WRYYYYYYYYYYYY"); 
-document.querySelector("#page-title").style.color = "red";
+let pageTitle = document.querySelector("#page-title");
+
+console.log("Initialized."); 
+
+
+pageTitle.style.color = "red";
+
+//the on click can work for specific attributes
+document.querySelector("header").onclick = function(){
+    console.log("clicked");
+    document.body.style.backgroundColor = "red";
+}
---
+++ class-exercises/tutorials/02-css/js/slide-in.js
@@ -0,0 +1,37 @@
+//Ensure JS runs after HMTL  content is loaded
+document.addEventListener("DOMContentLoaded", function () {
+    // Grab all the elements with class project-item
+    const projectItems = document.querySelectorAll(".project-item");
+
+    //Applies slide-in class to objects when they're visible to make them animate
+    function checkSlide() {
+        //Loop through the projectItems
+        projectItems.forEach((projectItem) => {
+            //Calculate the point where the top of the element is halfway into the viewport
+            const slideInAt = window.scrollY+window.innerHeight-projectItem.clientHeight/2;
+            
+            //Calculate the bottom of the project-item element
+            const itemBottom = projectItem.offsetTop + projectItem.clientHeight;
+            
+            //Check if the element is halfway shown or scrolled past
+            const isHalfShown = slideInAt > projectItem.offsetTop;
+            const isNotScrolledPast = window.scrollY < itemBottom;
+            
+            // if conditions are met, we add slide-in class else we remove it
+            if(isHalfShown && isNotScrolledPast){
+                projectItem.classList.add("slide-in");
+            } else {
+                projectItem.classList.remove("slide-in");
+            }            
+        })
+    }
+    
+    //Runs the checkSlide function on a page scroll or window resize
+    window.addEventListener("scroll", checkSlide);
+    window.addEventListener("resize", checkSlide);
+    
+    //Just for good measure
+    checkSlide();
+})
+
+console.log("Page Loaded!");
