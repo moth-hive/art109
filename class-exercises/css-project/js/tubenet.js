ef96ba8995503b729e743448397f9e6b218c6291
Solar-Moth <thaddeus.cobb@sjsu.edu>
2024-09-26 00:04:37 -0700

    final little changes

--- class-exercises/css-project/js/tubenet.js
+++ class-exercises/css-project/js/tubenet.js
@@ -3,16 +3,19 @@
 var posX = 0;
 var posY = 0;
 
-const backImg = document.querySelector('.map-img');
+
+const backImg = document.querySelector('.map-img'); /*WHY DO RETURN AS NULL?????*/
 
 document.addEventListener("DOMContentLoaded", () => {
     console.log("HEY HEY HEY! WE'RE LIVE!");
     
+    //Resets the css animation tag once it's done doing its thing
     document.querySelector('.map-img').addEventListener("animationend", () => document.querySelector('.map-img').style.animationName = '');
     
 });
 
 //Movement script for navigation. Pressing a button will move the scene and increment the position based on which button is pressed.
+//[TODO] Add slideout animations for the current image.
 function moveWindow(btnID){
     console.log(btnID);
     
